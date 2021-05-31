7e3f7194ca32a5370e42698cd03f56df18a04341
“polrb5” <polreigbroto@gmail.com>
2021-05-31 18:31:08 +0200

    Añadido el estado y el Twitter de los senyores

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import { senyores } from "../datos/senyores.js";
 
 const principal = document.querySelector(".principal");
 
-for (const { nombre, foto, profesion } of senyores) {
+for (const { nombre, foto, profesion, estado, twitter } of senyores) {
   const senyorElemento = document
     .querySelector(".senyor-molde")
     .cloneNode(true);
@@ -11,8 +11,12 @@ for (const { nombre, foto, profesion } of senyores) {
   nombreElemento.textContent = nombre;
   const imagen = senyorElemento.querySelector(".avatar > img");
   imagen.src = `img/${foto}`;
-  const senyorProfesion = senyorElemento.querySelector(".valor-dato");
+  const senyorProfesion = senyorElemento.querySelector(".profesion-dato");
   senyorProfesion.textContent = profesion;
+  const senyorEstado = senyorElemento.querySelector(".estado-dato");
+  senyorEstado.textContent = estado;
+  const senyorTwitter = senyorElemento.querySelector(".twitter-dato");
+  senyorTwitter.textContent = twitter;
 
   principal.append(senyorElemento);
 }
